646738ba2bbb94cfefc88f8b40c65931e3b9a3ae
50Fifty <jenseanfoo@gmail.com>
2023-03-20 13:15:58 +0800

    Created chat web app
    Basic functionalities: send and receive messages with simple storage.

--- server.py
+++ server.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from flask import render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'secret!'
+socketio = SocketIO(app)
+
+messages = []
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    print(messages)
+    return render_template('index.html', messages=messages)
+
+@socketio.on('connect')
+def test_connect():
+    print('Client connected:')
+
+@socketio.on('connected')
+def connected_event(json):
+    print('Received json: ' + str(json))
+
+@socketio.on('send message')
+def handle_message(json):
+    print('Received message: ' + str(json))
+    messages.append(json['data'])
+    socketio.emit('receive message', json)
+
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app, "0.0.0.0")
